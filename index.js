38380b780d0862aaa731be2bf1928880477618ad
Caroline kahuro <carolkahuro@gmail.com>
2023-07-18 22:29:22 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+
+
+function saturdayFun (activity="roller-skate") {
+    console.log (`This Saturday, I want to ${activity}`);
+    return `This Saturday, I want to ${activity}!`
+}
+saturdayFun ();
+
+const mondayWork = function (activity="go to the office") {
+    console.log(`This Monday, I will ${activity}`);
+    return `This Monday, I will ${activity}.`;
+}
+mondayWork ("work from home")
+
+function wrapAdjective (visualFlair = '*') {
+    const innerFunction = function (name="special") {
+        return `You are ${visualFlair}${name}${visualFlair}!`
+    }
+    return innerFunction;
+}
+console.log (wrapAdjective ("*") ("special"))
+
